b517b54c2960cc84ddb185f454818d1b78089b18
Neyvan <essiorho@gmail.com>
2020-08-01 22:40:18 +0300

    ready for snap test

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,13 +1,44 @@
-import javax.swing.JFrame
+import java.awt.BorderLayout
+import java.awt.EventQueue
+import java.awt.FlowLayout
+import javax.swing.*
 
-class App(title: String?) : JFrame(title)
-{
 
+class App(title: String) : JFrame() {
+
+    init {
+        createUI(title)
+    }
+
+    private fun createUI(title: String) {
+
+        setTitle(title)
+
+        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        setSize(300, 200)
+        setLocationRelativeTo(null)
+
+        val panel = JPanel()
+        panel.layout = BorderLayout()
+        add(panel)
+
+        val label = JLabel("Hello Snap World!")
+        val button = JButton("Say Hello!")
+        panel.add(label, BorderLayout.NORTH)
+        panel.add(button, BorderLayout.SOUTH)
+
+        button.addActionListener {
+            JOptionPane.showMessageDialog(this, "Hello!")
+        }
+    }
 }
 
-fun main()
-{
-    val app = App("Hello snap world!")
-    app.setSize(300, 300)
-    app.show()
+private fun createAndShowGUI() {
+
+    val frame = App("Simple")
+    frame.isVisible = true
+}
+
+fun main() {
+    EventQueue.invokeLater(::createAndShowGUI)
 }
